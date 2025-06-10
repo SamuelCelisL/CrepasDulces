1eaee354a3cc6aab3ebd61bec576b5d716dd8e59
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-06-10 18:23:22 -0500

    Ajustar la estructura de clases en el componente Descripcion para mejorar la disposición de los elementos

--- src/ui/App/Descripcion.tsx
+++ src/ui/App/Descripcion.tsx
@@ -2,7 +2,7 @@ export default function Descripcion() {
     return (
     <div className="flex flex-col items-center flex-wrap bg-[#EFE6DE]" >
         <h1 className="bg-[#E3CBAF] text-center font-bold text-2xl p-3 w-full">Descripcion</h1>
-        <div className="flex-row flex flex-wrap sm:flex-col md:flex-row">
+        <div className="flex flex-wrap flex-col md:flex-row sm:justify-center">
             <div className="m-4 p-2 shadow-lg rounded-4xl hover:shadow-2xl transition-shadow duration-300 transform hover:scale-105 bg-[#985d2f] hover:bg-[#9794f6] max-w-[250px]">
 
                 <p className="text-justify max-w-[700px] text-sm py-5 px-3 font-mono "><p className="text-center font-bold text-md">“Oh Lala Crepas” - Crepas Dulces</p>
